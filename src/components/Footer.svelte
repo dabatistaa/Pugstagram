5e0028e5c0d67bbc66a07144f22c3073ec07e210
dabatistaa <dabatistaa@gmail.com>
2020-06-09 07:00:16 -0500

    Pugstagram

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      <p>&copy {year} PUGSTAGRAM FROM PLATZI</p>
+    </div>
+  </div>
+</div>
